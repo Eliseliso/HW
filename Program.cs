add065a18326627ed47bd6dea5b7ff37bd756b84
Elis <eliso1190@yandex.ru>
2023-01-18 16:34:49 +0300

    First

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите N1: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Введите N2: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num >= num2)
+{
+    Console.Write("max =" + num);
+}
+else
+{
+    Console.Write("max =" + num2);
+}
